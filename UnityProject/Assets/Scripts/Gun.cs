7814911bd8e297aaf563c18f03801e9cbff4716d
Stefan Doychev <sdoychev@gmail.com>
2014-11-08 01:58:19 +0200

    - adjust shoot aim

--- UnityProject/Assets/Scripts/Gun.cs
+++ UnityProject/Assets/Scripts/Gun.cs
@@ -59,18 +59,22 @@ public class Gun : MonoBehaviour
 			anim.SetTrigger("Shoot");
 			audio.Play();
 
+			float v1,v2;
+			v1 = Input.GetAxis (AimHorizontalAxis);
+			v2 = Input.GetAxis (AimVerticalAxis);
+
 			// If the player is facing right...
 			if(playerCtrl.facingRight)
 			{
 				// ... instantiate the rocket facing right and set it's velocity to the right. 
 				Rigidbody2D bulletInstance = Instantiate(rocket, transform.position, Quaternion.Euler(new Vector3(0,0,0))) as Rigidbody2D;
-				bulletInstance.velocity = new Vector2(speed, 0);
+				bulletInstance.velocity = new Vector2(v1*speed, -v2*speed);
 			}
 			else
 			{
 				// Otherwise instantiate the rocket facing left and set it's velocity to the left.
 				Rigidbody2D bulletInstance = Instantiate(rocket, transform.position, Quaternion.Euler(new Vector3(0,0,180f))) as Rigidbody2D;
-				bulletInstance.velocity = new Vector2(-speed, 0);
+				bulletInstance.velocity = new Vector2(-speed*v1, -speed*v2);
 			}
 		}
 	}
