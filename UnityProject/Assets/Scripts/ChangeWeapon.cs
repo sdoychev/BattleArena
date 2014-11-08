d5be6175cdc0b43b637d6d0ac231d574f9be868d
Stefan Doychev <sdoychev@gmail.com>
2014-11-08 23:25:33 +0200

    ./. hero is now kroky

--- UnityProject/Assets/Scripts/ChangeWeapon.cs
+++ UnityProject/Assets/Scripts/ChangeWeapon.cs
@@ -3,20 +3,45 @@ using System.Collections;
 
 public class ChangeWeapon : MonoBehaviour {
 
-	Vector3 newPosition = new Vector3 (-1f, 2f, -2f);
+	private string AimHorizontalAxis = "AimHorizontal";
+	private string AimVerticalAxis = "AimVertical";
 
 	// Use this for initialization
 	void Start () {
 	
-		Debug.Log (gameObject.transform.name + " " + gameObject.transform.position);
-		//gameObject.transform.GetChild (0).GetComponent<MeshRenderer> ().enabled = true;
+		switch (gameObject.transform.parent.
+		        transform.parent.transform.parent.
+		        transform.parent.transform.parent.
+		        transform.parent.name) 
+		{
+		case "hero1":  
+			AimHorizontalAxis += "Player1";
+			AimVerticalAxis += "Player1";
+			break;
+		case "hero2":
+			AimHorizontalAxis += "Player2";
+			AimVerticalAxis += "Player2";
+			break;
+		case "hero3":
+			AimHorizontalAxis += "Player3";
+			AimVerticalAxis += "Player3";
+			break;
+		case "hero4":
+			AimHorizontalAxis += "Player4";
+			AimVerticalAxis += "Player4";
+			break;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		
+		float v1,v2;
+		v1 = Input.GetAxis (AimHorizontalAxis);
+		v2 = Input.GetAxis (AimVerticalAxis);
+		Vector2 v = new Vector2(v1, v2);
 
-		//gameObject.transform.GetChild (0).GetComponent<MeshRenderer> ().enabled = true;
-		gameObject.transform.position = gameObject.transform.position;
+		//gameObject.transform.LookAt(v. transform, Vector3.up);
 	
 	}
 }
