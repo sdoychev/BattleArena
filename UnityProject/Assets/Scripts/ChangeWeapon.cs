8f89857b74e65a308ae13627019a5bd284e91cf4
Sever4 <anton_sv86@mail.bg>
2014-11-08 15:43:55 +0200

    -

---
+++ UnityProject/Assets/Scripts/ChangeWeapon.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeWeapon : MonoBehaviour {
+
+	Vector3 newPosition = new Vector3 (-1f, 2f, -2f);
+
+	// Use this for initialization
+	void Start () {
+	
+		Debug.Log (gameObject.transform.name + " " + gameObject.transform.position);
+		//gameObject.transform.GetChild (0).GetComponent<MeshRenderer> ().enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//gameObject.transform.GetChild (0).GetComponent<MeshRenderer> ().enabled = true;
+		gameObject.transform.GetChild (0).transform.position = gameObject.transform.position;
+	
+	}
+}
