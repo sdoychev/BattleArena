56494c19696b23c82ac02987bb553a4c63f3fa7c
nqh <kofasbokluci@abv.bg>
2014-11-08 21:56:23 +0200

    _(_ gun stuff

--- UnityProject/Assets/Scripts/Gun.cs
+++ UnityProject/Assets/Scripts/Gun.cs
@@ -1,37 +1,31 @@
-﻿using UnityEngine;
+﻿﻿using UnityEngine;
 using System.Collections;
 
 public class Gun : MonoBehaviour
 {
 	public Rigidbody2D rocket;				// Prefab of the rocket.
 	public float speed = 20f;				// The speed the rocket will fire at.
-
-
+	public float fireRate;
+	private float fireTimer = 0f;
+	public bool applyForceToBullet;
+	
 	private PlayerControl playerCtrl;		// Reference to the PlayerControl script.
 	private Animator anim;					// Reference to the Animator component.
-
+	
 	private string FireButton = "Fire";
 	private string AimHorizontalAxis = "AimHorizontal";
 	private string AimVerticalAxis = "AimVertical";
-
-	private float fireRate { get; set; }
-	private float fireTimer{ set; get; }
-	private int damage{ set; get; }
-	private float bulletSpeed { set; get; }
-	private bool isBouncy { set; get; }
-	private bool hasGravity { set; get; }
-	private bool isAOE { set; get; }
-	private Rigidbody2D bullet { set; get; }
-
+	
+	
+	
+	
 	void Awake()
 	{
-		bullet = rocket; //hardcoded for testing purposes
-
 		// Setting up the references.
 		anim = transform.root.gameObject.GetComponent<Animator>();
 		playerCtrl = transform.root.GetComponent<PlayerControl>();
 		
-		switch (gameObject.transform.parent.transform.name) 
+		switch (gameObject.transform.parent.transform.parent.transform.name) 
 		{
 		case "hero1":  
 			FireButton += "Player1";
@@ -55,29 +49,34 @@ public class Gun : MonoBehaviour
 			break;
 		}
 	}
-
-
+	
+	
 	void Update ()
 	{
 		// If the fire button is pressed...
 		//if(Input.GetButtonDown(FireButton))
-		if( Input.GetAxisRaw(FireButton) != 0)
+		
+		fireTimer -= Time.deltaTime;
+		
+		if( Input.GetAxisRaw(FireButton) != 0  && fireTimer <= 0.1f)
 		{
+			fireTimer = fireRate;
+			
 			// ... set the animator Shoot trigger parameter and play the audioclip.
 			anim.SetTrigger("Shoot");
 			audio.Play();
-
+			
 			float v1,v2;
 			v1 = Input.GetAxis (AimHorizontalAxis);
 			v2 = Input.GetAxis (AimVerticalAxis);
-
-            Vector2 v = new Vector2(v1, v2);
-            v.Normalize();
-            v1 = v.x;
-            v2 = v.y;
-
-            
-
+			
+			Vector2 v = new Vector2(v1, v2);
+			v.Normalize();
+			v1 = v.x;
+			v2 = v.y;
+			
+			
+			
 			// If the player is facing right...
 			if(playerCtrl.facingRight)
 			{
@@ -88,7 +87,15 @@ public class Gun : MonoBehaviour
 				}
 				// ... instantiate the rocket facing right and set it's velocity to the right. 
 				Rigidbody2D bulletInstance = Instantiate(rocket, transform.position, Quaternion.Euler(new Vector3(0,0,0))) as Rigidbody2D;
-				bulletInstance.velocity = new Vector2(v1*speed, -v2*speed);
+				Vector2 vel = new Vector2(v1*speed, -v2*speed);
+				if (applyForceToBullet)
+				{
+					bulletInstance.AddForce(vel);
+				}
+				else
+				{
+					bulletInstance.velocity = vel;
+				}
 			}
 			else
 			{
@@ -97,10 +104,18 @@ public class Gun : MonoBehaviour
 					v1 = -1.0f;
 					v2 = 0.0f;
 				}
-
+				
 				// Otherwise instantiate the rocket facing left and set it's velocity to the left.
 				Rigidbody2D bulletInstance = Instantiate(rocket, transform.position, Quaternion.Euler(new Vector3(0,0,180f))) as Rigidbody2D;
-				bulletInstance.velocity = new Vector2(speed*v1, -speed*v2);
+				Vector2 vel = new Vector2(v1 * speed, -v2 * speed);
+				if (applyForceToBullet)
+				{
+					bulletInstance.AddForce(vel);
+				}
+				else
+				{
+					bulletInstance.velocity = vel;
+				}
 			}
 		}
 	}
