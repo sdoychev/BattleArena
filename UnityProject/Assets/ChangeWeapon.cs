45d753ff2717b3d1d309708e6d0a48b6df8a5daf
Stefan Doychev <sdoychev@gmail.com>
2014-11-08 03:53:15 +0200

    ./. add Kroky with animation

---
+++ UnityProject/Assets/ChangeWeapon.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeWeapon : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+		Debug.Log (gameObject.transform.name + " " + gameObject.transform.position);
+		//gameObject.transform.GetChild (0).GetComponent<MeshRenderer> ().enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
