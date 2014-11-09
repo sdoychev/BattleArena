8e52e0f1654dce8b3d75df7981f77dd63b403eda
Stefan Doychev <sdoychev@gmail.com>
2014-11-09 13:53:18 +0200

    ./. big fat UI

---
+++ UnityProject/Assets/Credits.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Credits : MonoBehaviour {
+
+	public Texture2D startButtonTexture;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI () {
+
+		GUI.backgroundColor = Color.clear;
+
+		if (GUI.Button(new Rect(Screen.width/2-440/2,0,384,150), startButtonTexture)) {
+			Application.LoadLevel ("Level");
+		}
+
+	}
+}
---
+++ UnityProject/Assets/UI.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI : MonoBehaviour {
+
+	public Texture2D controlTexture;
+	public Texture2D startButtonTexture;
+
+	public Texture2D creditsButtonTexture;
+	public Texture2D quitButtonTexture;
+
+	int offset = 88;
+	int voffset = 124;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI () {
+
+		GUI.backgroundColor = Color.clear;
+
+		if (GUI.Button(new Rect(Screen.width/2 - 256 + voffset,0 + offset,384,192), startButtonTexture)) {
+			Debug.Log("START GAME");
+			Application.LoadLevel ("Level");
+		}
+
+		if (GUI.Button(new Rect(Screen.width/2 - 256 + voffset,128 + offset,384,192), creditsButtonTexture)) {
+			Debug.Log("CREDITS");
+			Application.LoadLevel ("Credits");
+		}
+
+		if (GUI.Button(new Rect(Screen.width/2 - 256 + voffset,256 + offset,384,192), quitButtonTexture)) {
+			Debug.Log("QUIT");
+			Application.Quit();
+		}
+
+	}
+}
